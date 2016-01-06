0e62e8e6ff40d7c126f1c3e2d3605f16a4c84a9b
gingerbreadassassin <connordvn@gmail.com>
2016-01-05 22:41:44 -0600

    Added new argument to control method 'csvwrite', which takes a boolean value to enable/disable writing to a csv file.
    Also added an operation to change the setpoint, introducing 'differential spike' phenomenon

--- PID_Controller.py
+++ PID_Controller.py
@@ -1,3 +1,4 @@
+from distutils.util import strtobool
 import time
 from math import fabs
 from numpy import sign, log1p
@@ -5,7 +6,7 @@ from scipy.misc import derivative
 # import csv
 
 
-def measure(control, reading, iroc): # will take reading from sensor later, simulates temperature fn
+def measure(control, reading, iroc):  # will take reading from sensor later, simulates temperature fn
     return reading + iroc + control*0.15
 
 
@@ -25,31 +26,23 @@ def outputcalc(kp, ki, kd, error, integral, diffntl):
 
 
 class PID:
-
-    def control(self, setpoint, measurement, delta, kp, ki, kd, timeout):
-        # open csv and write header row
-        # with open('pid.csv', 'wb') as csvfile:
-        #     pidwriter = csv.writer(csvfile, delimiter=',',
-        #                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        #     pidwriter.writerow(['Interval', 'Delta', 'Target', 'Kp',
-        #                        'Ki', 'Kd', 'Measured',
-        #                        'Error1', 'Error2',
-        #                        'Integral', 'Differential', 'Output'])
-
-        # Get user input
-        # print("Welcome to the PID simpulator.")
-        # setpoint = float(input("Enter a desired temperature: "))
-        # measurement = float(input("Enter an initial measurement: "))
-        measure_prime = measurement
-        # delta = fabs(float(input("Enter a delta T (change in time between samples): ")))
-        # kp = float(input("Enter proportional gain: "))
-        # ki = float(input("Enter integral gain: "))
-        # kd = float(input("Enter differential gain: "))
+    @staticmethod
+    def control(setpoint, measurement, delta, kp, ki, kd, timeout, csvwrite):
+        if strtobool(csvwrite):
+            import csv
+            # open csv and write header row
+            with open('pid.csv', 'w', encoding='utf8', newline='') as csvfile:
+                pidwriter = csv.writer(csvfile, delimiter=',',
+                                       quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                pidwriter.writerow(['Interval', 'Delta', 'Target', 'Kp',
+                                   'Ki', 'Kd', 'Measured',
+                                    'Error1', 'Error2',
+                                    'Integral', 'Differential', 'Output'])
 
         # set up variables for bounded exponential growth formula y=C(1-e^(-kt))
         # note that t was arbitrarily chosen to equal 5.0 to calculate k
-        c = fabs(measure_prime - setpoint) * fabs(measure_prime - setpoint + 1.0) + 10.0
-        k = ((-1.0/5.0) * log1p((1.0-(measure_prime/c)-1.0)))
+        c = fabs(measurement - setpoint) * fabs(measurement - setpoint + 1.0) + 10.0
+        k = ((-1.0/5.0) * log1p((1.0-(measurement/c)-1.0)))
         e = 2.718281828
 
         # define bounded exponential growth formula
@@ -69,20 +62,29 @@ class PID:
             output = 100.0 * sign(output)
         round(output, 1)
 
-        # write values to csv
-        # pidwriter.writerow([interval, delta, setpoint, kp, ki, kd, measurement, err1, err2, integral, diffntl, output])
-        # output = 0.0
+        if strtobool(csvwrite):
+            # write values to csv
+            with open('pid.csv', 'a', encoding='utf8', newline='') as csvfile:
+                pidwriter = csv.writer(csvfile, delimiter=',',
+                                       quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                pidwriter.writerow([interval, delta, setpoint, kp, ki, kd, measurement, err1, err2, integral, diffntl,
+                                output])
         err1 = err2
         counter = 0
         timeout += time.time()
+        run_once = 0
 
         while counter < 100:
-            interval += delta # increments of user's chosen delta T
+            interval += delta  # increments of user's chosen delta T
+
+            if counter == 25 and run_once == 0:  # change setpoint after 25 'close' samples
+                setpoint += 5
+                run_once = 1
 
             # solve exponential function for t
             t1 = -log1p(1.0 - (measurement/c) - 1.0) / k
-            iroc = derivative(f, t1) # calculate instantaneous rate of change at t
-            measurement = measure(output, measurement, iroc) # simulate temperature measurement
+            iroc = derivative(f, t1)  # calculate instantaneous rate of change at t
+            measurement = measure(output, measurement, iroc)  # simulate temperature measurement
             err2 = calc_err(setpoint, measurement)
             integral += err1*delta
             diffntl = differential(err1, err2, delta)
@@ -90,7 +92,12 @@ class PID:
             if fabs(output) > 100.0:
                 output = 100.0 * sign(output)
 
-            # pidwriter.writerow([interval, delta, setpoint, kp, ki, kd, measurement, err1, err2, integral, diffntl, output])
+            if strtobool(csvwrite):
+                with open('pid.csv', 'a', encoding='utf8', newline='') as csvfile:
+                    pidwriter = csv.writer(csvfile, delimiter=',',
+                                       quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                    pidwriter.writerow([interval, delta, setpoint, kp, ki, kd, measurement, err1, err2, integral, diffntl,
+                                        output])
             err1 = err2
             if round(measurement, 2) == round(setpoint, 2):
                 counter += 1
@@ -100,11 +107,3 @@ class PID:
 
         # print("It took " + str(interval) + " seconds to stabilize.")
         return interval
-
-'''
-try:
-    main()
-
-except KeyboardInterrupt:
-    pass
-'''
